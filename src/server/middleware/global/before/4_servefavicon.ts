cc266b8933ed418172a4b6e19256e73742f39f2b
ashleybrener <ashley@starlogik.com>
2017-09-27 21:31:24 -0400

    Only serve favicon if file exists

--- src/server/middleware/global/before/4_servefavicon.ts
+++ src/server/middleware/global/before/4_servefavicon.ts
@@ -1,11 +1,19 @@
 import * as serveFavicon from 'serve-favicon';
-
+import { existsSync } from 'fs';
 import { Middleware, ExpressMiddlewareInterface } from 'routing-controllers';
 import { Request, Response, NextFunction } from 'express';
+
+const filePath = `${ process.cwd() }/public/favicon.ico`;
+const hasFavicon = existsSync(filePath);
 
 @Middleware({ type: 'before', priority: 4 })
 export class ServeFavicon implements ExpressMiddlewareInterface {
 	public use(req: Request, res: Response, next?: NextFunction): any {
-		serveFavicon(`${ process.cwd() }/public/favicon.ico`)(req, res, next);
+		if (hasFavicon) {
+			serveFavicon(filePath)(req, res, next);
+		}
+		else {
+			next();
+		}
 	}
 }
