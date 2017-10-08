bab2c1b33cd059efed275a9601cc82880033bdc6
ashleybrener <ashley@starlogik.com>
2017-10-08 15:26:12 -0400

    ws types were updated with breaking changes

--- src/server/socket/contracts/IWSServerOptions.ts
+++ src/server/socket/contracts/IWSServerOptions.ts
@@ -1,6 +1,6 @@
-import { IServerOptions } from 'ws';
+import { ServerOptions } from 'ws';
 import { ISigningOptions } from '../../contracts';
 
-export interface IWSServerOptions extends IServerOptions {
+export interface IWSServerOptions extends ServerOptions {
 	signing?: ISigningOptions;
 }
