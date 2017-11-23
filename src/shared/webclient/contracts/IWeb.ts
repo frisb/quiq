bca2dcf7f4ac832c9a29469775a56bfb339d88dd
ashleybrener <ashley@starlogik.com>
2017-11-22 22:07:06 -0500

    AuthToken type

--- src/shared/webclient/contracts/IWeb.ts
+++ src/shared/webclient/contracts/IWeb.ts
@@ -1,8 +1,9 @@
 import { IWebClient } from './IWebClient';
 import { IJsonWebClient } from './IJsonWebClient';
 import { IAuthWebClient } from './IAuthWebClient';
+import { AuthToken } from '../index';
 
-export interface IWeb<TAuthToken> {
+export interface IWeb<TAuthToken extends AuthToken> {
 	WebClient: { new(userAgent?: string): IWebClient };
 	JsonWebClient: { new(userAgent?: string): IJsonWebClient };
 	AuthWebClient: { new(userAgent?: string): IAuthWebClient<TAuthToken> };
