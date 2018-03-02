c213db41ed92659c3f69b50f0795012565a0068e
ashleybrener <ashley@starlogik.com>
2018-03-02 17:40:34 -0500

    updated ipaddr.js typings

--- typings/index.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference path="ipaddr.js.d.ts" />
-
-export * from '../src/index';
--- typings/ipaddr.js.d.ts
+++
@@ -1,7 +0,0 @@
-declare module 'ipaddr.js' {
-	interface IPv4 {
-		toString(): string;
-	}
-	
-	export function process(ip: string): IPv4;
-}
