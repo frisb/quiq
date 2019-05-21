ac979fecf7b50a4d4d3b1c26682cd547cab02af6
ashleybrener <ashley@starlogik.com>
2019-05-21 22:56:49 +0200

    final middleware

---
+++ src/server/middleware/global/after/4_final.ts
@@ -0,0 +1,9 @@
+import { Middleware, ExpressMiddlewareInterface } from 'routing-controllers';
+import { Request, Response, NextFunction } from 'express';
+
+@Middleware({ type: 'after', priority: 4 })
+export class Final implements ExpressMiddlewareInterface {
+	public use(req: Request, res: Response, next?: NextFunction): any {
+		// no next() called.
+	}
+}
---
+++ src/server/socket/abstract/server/types.ts_
@@ -0,0 +1,4 @@
+export enum serverTypes {
+	WS,
+	SOCKET_IO
+}
